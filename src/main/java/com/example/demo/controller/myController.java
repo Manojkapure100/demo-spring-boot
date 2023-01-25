9d67eeacba3c684782de516b1e396ed2f1bfffac
Manojkapure100 <manojkapure100@gmail.com>
2023-01-25 11:32:28 +0530

    new functions added

--- src/main/java/com/example/demo/controller/myController.java
+++ src/main/java/com/example/demo/controller/myController.java
@@ -1,20 +1,32 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class myController {
     
-    @GetMapping("/check")
-    public String myFunction(@RequestParam String msg){
+    @GetMapping("/")
+    public String myFunction(){
+        return "My Controller Running...";
+    }
+
+    @GetMapping("/check1")
+    public String myFunction1(@RequestParam String msg){
         return "Messange in url is: "+msg;
     }
 
     @GetMapping("/check2")
     public String function2(){
         return "function 2 is calling without url parameters";
+    }
+
+    @GetMapping("/check3")
+    public String function3(@RequestParam int count){
+        return "function 3 is calling with url parameters: "+count;
     }
 
 }
